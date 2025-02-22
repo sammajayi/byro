10760b840943bb70c1375851eb4bb3103b3cc47e
Samuel Ajayi <samuelajayi554@gmail.com>
2025-02-22 23:58:20 +0100

    updated the footer design

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,8 @@ import Link from 'next/link'
 const Footer = () => {
     return (
         <footer>
-            <div className='flex p-8 space-x-10'>
+            <div className='flex flex-col justify-center items-center'>
+            <div className='flex p-8 space-x-32 justify-center items-center'>
             <div className='space-y-4'>
                 <div className='logo'>
                     <Link href="/">
@@ -14,7 +15,7 @@ const Footer = () => {
                 </div>
                 
                 <div>
-                    <p>
+                    <p className='text-sm font-normal'>
                         Seamless, Secure, and Decentralized Event Ticketing
                     </p>
                 </div>
@@ -38,30 +39,33 @@ const Footer = () => {
                 </div>
             </div>
 
-                <div className='flex space-x-20'>
+                <div className='flex space-x-20 '>
                     <div>
-                        <h5>About Us</h5>
+                        <h5 className='text-lg font-bold mb-5'>About Us</h5>
                         <Link href="/">
-                        <p>Terms of Use</p>
+                        <p className='text-gray-400 text-sm mb-2'>Terms of Use</p>
                         </Link>
 
                         <Link href="/">
-                        <p>Service Policy</p>
+                        <p className='text-gray-400 text-sm'>Service Policy</p>
                         </Link>
                     </div>
 
                     <div>
-                        <h5>Services</h5>
+                        <h5 className='text-lg font-bold mb-5'>Services</h5>
                         <Link href="/">
-                        <p>Event Ticketing</p>
+                        <p className='text-gray-400 text-sm mb-2'>Event Ticketing</p>
                         </Link>
 
                         <Link href="/">
-                        <p>Gaming</p>
+                        <p className='text-gray-400 text-sm'>Gaming</p>
                         </Link>
                     </div>
                 </div>
+            </div>
 
+            <hr className='border-t-2 border-gray-200 w-[80%]' />
+            <p className='text-gray-400 text-xs text-center p-5'>&#169; Byro Africa. All Rights Reserved.</p>
             </div>
         </footer>
     )
