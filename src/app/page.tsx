3ff19b6fd3896965eb9f46d43c13d66bab8dace5
Samuel Ajayi <samuelajayi554@gmail.com>
2025-04-24 20:41:36 +0100

    added the footer to the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@ import HeroPage from './components/HeroPage';
 import EventSteps from './components/EventSteps';
 import RegisterSteps from './components/RegisterSteps';
 
+
 export default function Home() {
   return (
     <>
      <HeroPage />
      <RegisterSteps />
      <EventSteps />
-    {/* <Footer /> */}
+    <Footer />
     </>
    
   );
