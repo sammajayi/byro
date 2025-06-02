ada28067631cff342592170e57e6d7c270f0c90c
Samuel Ajayi <samuelajayi554@gmail.com>
2025-06-02 10:39:56 +0100

    kk

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -186,7 +186,7 @@ const ViewEvent = () => {
         
         <main className="mx-auto p-4 md:p-6 lg:p-10 w-full lg:w-[80%] xl:w-[70%] 2xl:w-[60%] ">
           
-          <div className="flex flex-col md:flex-row gap-6 md:gap-10 lg:gap-14 justify-center items-start pt-32">
+          <div className="flex flex-col md:flex-row gap-6 md:gap-10 lg:gap-14 justify-center items-start pt-20">
             {/* Left column */}
             <div className="w-full md:w-auto">
               {/* Event image */}
