4e58095d45b795242852e2bef52b4efbd72024af
Samuel Ajayi <samuelajayi554@gmail.com>
2025-06-20 12:08:03 +0100

    removed the delcared and unused path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import EventSteps from "../components/EventSteps";
 import RegisterSteps from "../components/RegisterSteps";
 // import RegisterModal from "../components/auth/RegisterModal"
 import HappeningEvents from "../components/HappeningEvents";
-import App from "next/app";
+// import App from "next/app";
 import AppLayout from "@/layout/app";
 
 export default function Home() {
